deee7905c431ebdc436ff7c862264ca9cac06166
bkuchel <100967866+bkuchel@users.noreply.github.com>
2022-04-01 00:12:12 +0200

    interfaz selección de sala

--- server.js
+++ server.js
@@ -42,40 +42,40 @@ const server = http.createServer((req, res) => {
     }
 
     // // Build file path
-    // let filePath = path.join(
-    //     __dirname,
-    //     "public/views/",
-    //     req.url === "/" ? "index.html" : req.url
-    // );
+     let filePath = path.join(
+         __dirname,
+         "public/views/",
+         req.url === "/" ? "index.html" : req.url
+     );
     //
     // // Extension of file
-    // let extname = path.extname(filePath) ;
+     let extname = path.extname(filePath) ;
     //
     //
     // // Initial content type
-    // let contentType = "text/html";
+     let contentType = "text/html";
     //
     // // Check ext and set content type
-    // switch (extname) {
-    //     case ".js":
-    //         contentType = "text/javascript";
-    //         break;
-    //     case ".css":
-    //         contentType = "text/css";
-    //         break;
-    //     case ".json":
-    //         contentType = "application/json";
-    //         break;
-    //     case ".png":
-    //         contentType = "image/png";
-    //         break;
-    //     case ".jpg":
-    //         contentType = "image/jpg";
-    //         break;
-    // }
+     switch (extname) {
+         case ".js":
+             contentType = "text/javascript";
+             break;
+         case ".css":
+             contentType = "text/css";
+            break;
+         case ".json":
+             contentType = "application/json";
+             break;
+         case ".png":
+             contentType = "image/png";
+             break;
+         case ".jpg":
+             contentType = "image/jpg";
+             break;
+     }
     //
     // // Check if contentType is text/html but no .html file extension
-    // if (contentType == "text/html" && extname == "") filePath += ".html";
+     if (contentType == "text/html" && extname == "") filePath += ".html";
     //
     // // log the filePath
     // console.log(filePath);
@@ -83,28 +83,28 @@ const server = http.createServer((req, res) => {
     //
     //
     // // Read File
-    // fs.readFile(filePath, (err, content) => {
-    //     if (err) {
-    //         if (err.code == "ENOENT") {
-    //             // Page not found
-    //             fs.readFile(
-    //                 path.join(__dirname, "public", "404.html"),
-    //                 (err, content) => {
-    //                     res.writeHead(404, { "Content-Type": "text/html" });
-    //                     res.end(content, "utf8");
-    //                 }
-    //             );
-    //         } else {
-    //             //  Some server error
-    //             res.writeHead(500);
-    //             res.end(`Server Error: ${err.code}`);
-    //         }
-    //     } else {
-    //         // Success
-    //         res.writeHead(200, { "Content-Type": contentType });
-    //         res.end(content, "utf8");
-    //     }
-    // });
+     fs.readFile(filePath, (err, content) => {
+         if (err) {
+             if (err.code == "ENOENT") {
+                 // Page not found
+                 fs.readFile(
+                     path.join(__dirname, "public", "404.html"),
+                     (err, content) => {
+                         res.writeHead(404, { "Content-Type": "text/html" });
+                         res.end(content, "utf8");
+                     }
+                 );
+             } else {
+                 //  Some server error
+                 res.writeHead(500);
+                 res.end(`Server Error: ${err.code}`);
+             }
+         } else {
+             // Success
+             res.writeHead(200, { "Content-Type": contentType });
+             res.end(content, "utf8");
+         }
+     });
 
 
 });
