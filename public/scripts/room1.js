0c257656da342df400645706c05868fd3fb709aa
JuanCarlosRoman <92890844+JuanCarlosRoman@users.noreply.github.com>
2022-04-04 01:05:41 +0200

    Update room1.js

--- public/scripts/room1.js
+++ public/scripts/room1.js
@@ -1,7 +1,7 @@
 
 function chStar() {
     let elem = document.querySelector(".estrella")
-    elem.innerHTML = '<i id="favorite" class="far fa-star fa-2x"></i>';
+    elem.innerHTML = '<i id="favorite" class="fas fa-star fa-2x"></i>';
     console.log(elem)
 
 }
